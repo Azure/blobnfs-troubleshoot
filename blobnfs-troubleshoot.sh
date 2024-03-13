426a1da4ec570c93d3a697d43f11660164a2b41c
smb007 <shankarmb@microsoft.com>
2024-03-13 11:33:32 +0530

    small update

--- blobnfs-troubleshoot.sh
+++ blobnfs-troubleshoot.sh
@@ -6,10 +6,14 @@ if [ $(id -u) -ne 0 ]; then
     exit 1
 fi
 
+# Default branch name
+branchname="develop"
+
 # Check for the number of parameters to the script
-if [[ $# != 2 ]]; then
+if [[ $# -le 2 ]]; then
     read -p "Enter the absolute mount path: " absmountpoint
     read -p "Enter the server IP: " serverip
+    read -p "[Optional] Enter the branch name (default: $branchname): " branchname
 
     # If parameters are not provided, then exit
     if [[ -z $absmountpoint || -z $serverip ]]; then
@@ -21,7 +25,7 @@ fi
 filepath="/tmp/ubuntu-troubleshoot.sh"
 # Download the latest troubleshooting script
 echo "Downloading the latest troubleshooting script"
-wget https://raw.githubusercontent.com/Azure/blobnfs-troubleshoot/main/ubuntu-troubleshoot.sh -O $filepath
+wget "https://raw.githubusercontent.com/Azure/blobnfs-troubleshoot/$branchname/ubuntu-troubleshoot.sh" -O $filepath
 
 # Make the script executable
 chmod +x $filepath
@@ -40,4 +44,4 @@ echo "Share the log file $logfile with the support team."
 # Populate absmountpoint and serverip
 # absmountpoint=""; serverip=""; logfile="/tmp/troubleshoot$$.log"; > $logfile; ./ubuntu-troubleshoot.sh "$absmountpoint" "$serverip" |& tee -a $logfile; echo "Share the log file $logfile with the support team."
 
-# curl -s https://raw.githubusercontent.com/Azure/blobnfs-troubleshoot/main/ubuntu-troubleshoot.sh | bash -s "<absolute mount path>" "<server ip>"
+# curl -s https://raw.githubusercontent.com/Azure/blobnfs-troubleshoot/develop/ubuntu-troubleshoot.sh | bash -s "<absolute mount path>" "<server ip>"
